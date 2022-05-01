5568dfae1513ee2acdac58c0324acba12b4d8783
Diogenesoftoronto <87236699+Diogenesoftoronto@users.noreply.github.com>
2022-04-30 23:42:09 -0400

    basic setup for bot

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-const discord = require('discord.js')
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const token = ""
+var inviteLink = "";
+
+client.on("channelCreate", function(channel){
+  console.log(`channelCreate: ${channel}`);
+});
