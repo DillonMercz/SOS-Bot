6978f99729988ec235dfcc6348a9a0c350e18d5f
Diogenesoftoronto <87236699+Diogenesoftoronto@users.noreply.github.com>
2022-05-01 02:32:26 -0400

    add requires and socket connections on port

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
+require('dotenv').config;
 const Discord = require('discord.js');
 const client = new Discord.Client();
-require('dotenv').config;
-const token = process.env.DISCORD_TOKEN
-var inviteLink = "";
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require("socket.io")(server);
+const port = process.env.PORT || 8080;
+const listenfunc = () => console.log('server running');
+const discordToken = process.env.DISCORD_TOKEN
+const inviteLink = "";
+const connectionwssfunc = () => console.log('user is connected')
+
 
 client.on("channelCreate", function(channel){
   console.log(`channelCreate: ${channel}`);
 });
+
+io.on('connection', connectionwssfunc)
+
+server.listen(port, listenfunc)
