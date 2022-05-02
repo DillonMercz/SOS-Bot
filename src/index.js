d0f85b770ebc27c58fd4f03669b2000dcd5b4759
leiandros <leiandros@hotmail.com>
2022-05-01 23:24:17 -0300

    added token for testing

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-const discord = require('discord.js')
+const { Client, Intents } = require('discord.js');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+
+client.once('Ready', () =>{
+    console.log('SOS-bot online');
+});
+
+client.login('OTcwNDgyMDAyNzYxNTEwOTky.GwYjqz.tboaTowHzpZyqwu1b3Rkh1p6xBxNip2EzKn7o4');
+
+
