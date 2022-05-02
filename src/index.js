5b1bd72d3eeb17d1c8ed17e217775fd4e0fac2db
Diogenesoftoronto <87236699+Diogenesoftoronto@users.noreply.github.com>
2022-05-02 05:24:27 -0400

    clean up format and add comments

--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@ const { WebSocketServer } = require("ws");
 // const client = new Discord.Client();
 const ws = require("ws");
 const app = require("express")();
-const server = require("http").Server(app);
+const server = require("http").createServer(app);
 // const io = require("socket.io")(server);
 const twilioConfig = require("./twilioConfig");
 const wss = new ws.Server({ server });
+
+
 // constants
-const port = process.env.PORT || 8080;
+const PORT = process.env.SERVER_PORT || 8080;
 const discordToken = process.env.DISCORD_TOKEN;
 const inviteLink = "";
-// commands
+
+// discord bot commands
 const PREFIX = "*";
 const _CMD_HELP = PREFIX + "help";
 const _CMD_END_CALL = PREFIX + "endcall";
@@ -21,7 +24,8 @@ const _CMD_DEBUG = PREFIX + "debug";
 const _CMD_TEST = PREFIX + "test";
 const _CMD_RESET = PREFIX + "reset";
 const _CMD_CONVOLOG = PREFIX + "convo";
-// help function
+
+// help discord bot function
 const getHelp = () =>
   `Usage: *[COMMAND] [ARGUMENTS]\n displays this help message: ${_CMD_HELP}\n ends call of the user \n ${_CMD_END_CALL} [USER]\n displays the conversation logs of the user ${_CMD_CONVOLOG} [USER] \n`;
 const guildMap = new Map();
@@ -31,7 +35,7 @@ let voiceConnection;
 //   console.log(`channelCreate: ${start}`);
 // });
 
-// connection callback
+// websocket connection callback
 const connectionWssFunc = (ws) => {
   console.log("New connection Established");
   let recognizeStream = null;
@@ -45,37 +49,54 @@ const connectionWssFunc = (ws) => {
       case "start":
         console.log(`Starting Media Stream ${msg.streamSid}`);
         // Create Stream to the Google Speech to Text API
-        recognizeStream = client
-          .streamingRecognize(request)
-          .on("error", console.error)
-          .on("data", (data) => {
-            console.log(data.results[0].alternatives[0].transcript);
-            wss.clients.forEach((client) => {
-              if (client.readyState === WebSocket.OPEN) {
-                client.send(
-                  JSON.stringify({
-                    event: "interim-transcription",
-                    text: data.results[0].alternatives[0].transcript,
-                  })
-                );
-              }
-            });
-          });
-        break;
+        // recognizeStream = client
+        //   .streamingRecognize(request)
+        //   .on("error", console.error)
+        //   .on("data", (data) => {
+        //     console.log(data.results[0].alternatives[0].transcript);
+        //     wss.clients.forEach((client) => {
+        //       if (client.readyState === WebSocket.OPEN) {
+        //         client.send(
+        //           JSON.stringify({
+        //             event: "interim-transcription",
+        //             text: data.results[0].alternatives[0].transcript,
+        //           })
+        //         );
+        //       }
+        //     });
+        //   });
+        // break;
       case "media":
         // Write Media Packets to the recognize stream
         console.log(`Audio being Recieved...`);
-        recognizeStream.write(msg.media.payload);
+        // recognizeStream.write(msg.media.payload);
         break;
       case "stop":
         console.log(`Call Has Ended`);
-        recognizeStream.destroy();
+        // recognizeStream.destroy();
         break;
     }
   });
 };
 // io.on('connection', connectionwssfunc)
 // listen callback
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/index.html")));
+
+app.post("/", (req, res) => {
+  res.set("Content-Type", "text/xml");
+
+  res.send(`
+    <Response>
+      <Start>
+        <Stream url="wss://${req.headers.host}/"/>
+      </Start>
+      <Say>I will stream the next 60 seconds of audio through your websocket</Say>
+      <Pause length="60" />
+    </Response>
+  `);
+});
+
 wss.on("connection", connectionWssFunc);
-const listenfunc = () => console.log("server running");
-server.listen(port, listenfunc);
+// 
+const listenfunc = () => console.log("server running on "+port);
+server.listen(PORT, listenfunc);
