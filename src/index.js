c73a033436ada92a3a160b205aecce0c43a67790
Diogenesoftoronto <87236699+Diogenesoftoronto@users.noreply.github.com>
2022-05-03 03:55:25 -0400

    trailing comma from hell eric helps eliminate

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 require("dotenv").config;
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const { WebSocketServer } = require("ws");
 // const client = new Discord.Client();
 const ws = require("ws");
-const app = require("express")();  
+const app = require("express")();
 const server = require("http").createServer(app);
 // const io = require("socket.io")(server);
-const { OpusEncoder } = require('@discordjs/opus');
+const { OpusEncoder } = require("@discordjs/opus");
 const twilioConfig = require("./twilioConfig");
 const wss = new ws.Server({ server });
 const path = require("path");
-const {PassThrough} = require('stream')
+const { PassThrough } = require("stream");
 const TwilioMediaStreamSaveAudioFile = require("twilio-media-stream-save-audio-file");
 
 const mediaStreamSaver = new TwilioMediaStreamSaveAudioFile({
@@ -35,7 +35,7 @@ const _CMD_CONVOLOG = PREFIX + "convo";
 
 // help discord bot function
 const getHelp = () =>
-`Usage: *[COMMAND] [ARGUMENTS]\n displays this help message: ${_CMD_HELP}\n ends call of the user \n ${_CMD_END_CALL} [USER]\n displays the conversation logs of the user ${_CMD_CONVOLOG} [USER] \n`;
+  `Usage: *[COMMAND] [ARGUMENTS]\n displays this help message: ${_CMD_HELP}\n ends call of the user \n ${_CMD_END_CALL} [USER]\n displays the conversation logs of the user ${_CMD_CONVOLOG} [USER] \n`;
 const guildMap = new Map();
 let voiceConnection;
 
@@ -66,43 +66,103 @@ const connectionWssFunc = (ws) => {
       case "start":
         console.log(`Starting Media Stream ${msg.streamSid}`);
         let streamSid = msg.start.streamSid;
-        ws.wstream = fs.createWriteStream(__dirname + `/${Date.now()}.wav`, { encoding: 'binary' });
+        ws.wstream = fs.createWriteStream(__dirname + `/${Date.now()}.wav`, {
+          encoding: "binary",
+        });
         // This is a mu-law header for a WAV-file compatible with twilio format
-        ws.wstream.write(Buffer.from([
-          0x52,0x49,0x46,0x46,0x62,0xb8,0x00,0x00,0x57,0x41,0x56,0x45,0x66,0x6d,0x74,0x20,
-          0x12,0x00,0x00,0x00,0x07,0x00,0x01,0x00,0x40,0x1f,0x00,0x00,0x80,0x3e,0x00,0x00,
-          0x02,0x00,0x04,0x00,0x00,0x00,0x66,0x61,0x63,0x74,0x04,0x00,0x00,0x00,0xc5,0x5b,
-          0x00,0x00,0x64,0x61,0x74,0x61,0x00,0x00,0x00,0x00, // Those last 4 bytes are the data length
-        ]))
-        ws.rstream = fs.createReadStream(_dirname + `/${Date.now()}.wav`, { encoding: 'binary' })
+        ws.wstream.write(
+          Buffer.from([
+            0x52,
+            0x49,
+            0x46,
+            0x46,
+            0x62,
+            0xb8,
+            0x00,
+            0x00,
+            0x57,
+            0x41,
+            0x56,
+            0x45,
+            0x66,
+            0x6d,
+            0x74,
+            0x20,
+            0x12,
+            0x00,
+            0x00,
+            0x00,
+            0x07,
+            0x00,
+            0x01,
+            0x00,
+            0x40,
+            0x1f,
+            0x00,
+            0x00,
+            0x80,
+            0x3e,
+            0x00,
+            0x00,
+            0x02,
+            0x00,
+            0x04,
+            0x00,
+            0x00,
+            0x00,
+            0x66,
+            0x61,
+            0x63,
+            0x74,
+            0x04,
+            0x00,
+            0x00,
+            0x00,
+            0xc5,
+            0x5b,
+            0x00,
+            0x00,
+            0x64,
+            0x61,
+            0x74,
+            0x61,
+            0x00,
+            0x00,
+            0x00,
+            0x00, // Those last 4 bytes are the data length
+          ])
+        );
+        ws.rstream = fs.createReadStream(_dirname + `/${Date.now()}.wav`, {
+          encoding: "binary",
+        });
         buffer = ws.rstream;
         encoded = encoder.encode(buffer);
         decoded = encoder.decode(encoded);
-      // Create Stream to the Google Speech to Text API
-      // recognizeStream = client
-      //   .streamingRecognize(request)
-      //   .on("error", console.error)
-      //   .on("data", (data) => {
-      //     console.log(data.results[0].alternatives[0].transcript);
-      //     wss.clients.forEach((client) => {
-      //       if (client.readyState === WebSocket.OPEN) {
-      //         client.send(
-      //           JSON.stringify({
-      //             event: "interim-transcription",
-      //             text: data.results[0].alternatives[0].transcript,
-      //           })
-      //         );
-      //       }
-      //     });
-      //   });
-      break;
+        // Create Stream to the Google Speech to Text API
+        // recognizeStream = client
+        //   .streamingRecognize(request)
+        //   .on("error", console.error)
+        //   .on("data", (data) => {
+        //     console.log(data.results[0].alternatives[0].transcript);
+        //     wss.clients.forEach((client) => {
+        //       if (client.readyState === WebSocket.OPEN) {
+        //         client.send(
+        //           JSON.stringify({
+        //             event: "interim-transcription",
+        //             text: data.results[0].alternatives[0].transcript,
+        //           })
+        //         );
+        //       }
+        //     });
+        //   });
+        break;
       case "media":
         // Write Media Packets to the recognize stream
         console.log(`Audio being Recieved...`);
-        payload = msg.media.payload
-        ws.wstream.write(Buffer.from(payload, 'base64'));
+        payload = msg.media.payload;
+        ws.wstream.write(Buffer.from(payload, "base64"));
         // output the variables for view
-        console.table({buffer, encoded, decoded, payload})
+        console.table({ buffer, encoded, decoded, payload });
         // recognizeStream.write(msg.media.payload);
         break;
       case "stop":
@@ -134,6 +194,3 @@ wss.on("connection", connectionWssFunc);
 //
 const listenfunc = () => console.log("server running on " + PORT);
 server.listen(PORT, listenfunc);
-
-
-
