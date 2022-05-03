bea99bc1bb4b453079f0b974dd1e335071a99360
Diogenesoftoronto <87236699+Diogenesoftoronto@users.noreply.github.com>
2022-05-03 10:36:13 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ const wss = new ws.Server({ server });
 const path = require("path");
 const { PassThrough } = require("stream");
 const TwilioMediaStreamSaveAudioFile = require("twilio-media-stream-save-audio-file");
+// const __dirname = './twilio-audio'
 // currently not used
 const mediaStreamSaver = new TwilioMediaStreamSaveAudioFile({
   saveLocation: __dirname,
@@ -68,6 +69,7 @@ const connectionWssFunc = (ws) => {
   let recognizeStream = null;
   let payload;
   let obj;
+  mediaStreamSaver.setWebsocket(ws);
   ws.on("message", function incoming(message) {
     const msg = JSON.parse(message);
     switch (msg.event) {
@@ -76,73 +78,7 @@ const connectionWssFunc = (ws) => {
         break;
       case "start":
         console.log(`Starting Media Stream ${msg.streamSid}`);
-        let streamSid = msg.start.streamSid;
-        ws.wstream = fs.createWriteStream(__dirname + `/${Date.now()}.wav`, {
-          encoding: "binary",
-        });
-        // This is a mu-law header for a WAV-file compatible with twilio format
-        ws.wstream.write(
-          Buffer.from([
-            0x52,
-            0x49,
-            0x46,
-            0x46,
-            0x62,
-            0xb8,
-            0x00,
-            0x00,
-            0x57,
-            0x41,
-            0x56,
-            0x45,
-            0x66,
-            0x6d,
-            0x74,
-            0x20,
-            0x12,
-            0x00,
-            0x00,
-            0x00,
-            0x07,
-            0x00,
-            0x01,
-            0x00,
-            0x40,
-            0x1f,
-            0x00,
-            0x00,
-            0x80,
-            0x3e,
-            0x00,
-            0x00,
-            0x02,
-            0x00,
-            0x04,
-            0x00,
-            0x00,
-            0x00,
-            0x66,
-            0x61,
-            0x63,
-            0x74,
-            0x04,
-            0x00,
-            0x00,
-            0x00,
-            0xc5,
-            0x5b,
-            0x00,
-            0x00,
-            0x64,
-            0x61,
-            0x74,
-            0x61,
-            0x00,
-            0x00,
-            0x00,
-            0x00, // Those last 4 bytes are the data length
-          ])
-        );
+        mediaStreamSaver.twilioStreamStart();
         try{
           ws.rstream = fs.createReadStream(__dirname + `/${Date.now()}.wav`);
           buffer = readChunks(ws.rstream);
@@ -152,64 +88,25 @@ const connectionWssFunc = (ws) => {
         catch(e){
           console.log(e)
         }
-        // Create Stream to the Google Speech to Text API
-        // recognizeStream = client
-        //   .streamingRecognize(request)
-        //   .on("error", console.error)
-        //   .on("data", (data) => {
-        //     console.log(data.results[0].alternatives[0].transcript);
-        //     wss.clients.forEach((client) => {
-        //       if (client.readyState === WebSocket.OPEN) {
-        //         client.send(
-        //           JSON.stringify({
-        //             event: "interim-transcription",
-        //             text: data.results[0].alternatives[0].transcript,
-        //           })
-        //         );
-        //       }
-        //     });
-        //   });
         break;
       case "media":
-        // Write Media Packets to the recognize stream
-        console.log(`Audio being Recieved...`);
-        payload = msg.media.payload;
-        ws.wstream.write(Buffer.from(payload, "base64"));
+        console.log("Receiving audio...");
+        mediaStreamSaver.twilioStreamMedia(msg.media.payload);
         try{
           ws.rstream = fs.createReadStream(__dirname + `/${Date.now()}.wav`);
           buffer = readChunks(ws.rstream);
           encoded = encoder.encode(buffer);
           decoded = encoder.decode(encoded);
         }
-        catch(e) {
+        catch(e){
           console.log(e)
         }
-        // output the variables for view
-        obj = { buffer, encoded, decoded, payload };
-        console.table(obj);
-        // recognizeStream.write(msg.media.payload);
         break;
       case "stop":
-        console.log(`Call Has Ended`);
-        // recognizeStream.destroy();
-        ws.wstream.write("", () => {
-          let fd = fs.openSync(ws.wstream.path, "r+"); // `r+` mode is needed in order to write to arbitrary position
-          let count = socket.wstream.bytesWritten;
-          count -= 58; // The header itself is 58 bytes long and we only want the data byte length
-          console.log(count);
-          fs.writeSync(
-            fd,
-            Buffer.from([
-              count % 256,
-              (count >> 8) % 256,
-              (count >> 16) % 256,
-              (count >> 24) % 256,
-            ]),
-            0,
-            4, // Write 4 bytes
-            54 // starts writing at byte 54 in the file
-          );
-        });
+        console.log("Call has ended");
+        mediaStreamSaver.twilioStreamStop();
+        break;
+      default:
         break;
     }
   });
